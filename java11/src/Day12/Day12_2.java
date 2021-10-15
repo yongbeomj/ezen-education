9b3bfb893464132f16c82498164bc908746809cb
JYB <alex.ybjang@gmail.com>
2021-10-15 11:06:17 +0900

    Day12

--- java11/src/Day12/Day12_2.java
+++ java11/src/Day12/Day12_2.java
@@ -2,4 +2,25 @@ package Day12;
 
 public class Day12_2 {
 
+	// p.527
+		// Wrapper 포장 클래스 : 기본자료형 -> 객체화
+	
+	// p.531
+	// 문자열 -> 기본타입
+		// 1. Integer.parseInt("문자열");
+		// 2. Double.parseDouble("문자열");
+		// 3. Boolean.parseBoolean("문자열");
+		// * Byte.parseByte("문자열"); , Short.parseShort("문자열"); , Long.parseLong("문자열"); , Float.parseFloat("문자열");
+	// 기본타입 -> 문자열
+		// 1. String.valueOf(기본자료형)
+		// 바이트배열 -> 문자열
+		// 2. new 생성자(바이트배열)
+	
+	
+	
+	public static void main(String[] args) {
+
+	
+	
+	}
 }
