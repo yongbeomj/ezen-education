c23c68a2fde1b5e31c94b4164e287944322648ff
JYB <alex.ybjang@gmail.com>
2021-10-28 13:09:28 +0900

    수정

--- java11/src/Evaluation/Main.java
+++ java11/src/Evaluation/Main.java
@@ -12,6 +12,7 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		
 		ArrayList<Student> treeSet = new ArrayList<>();
+		TreeSet<Student> treeSet2 = new TreeSet<>();
 		
 		try {
 			while(true) {
@@ -24,11 +25,13 @@ public class Main {
 					System.out.print("수학점수 : "); int mat = scanner.nextInt();
 					// treeset 추가
 					treeSet.add(new Student(name, kor, eng, mat));
+					treeSet2.add(new Student(name, kor, eng, mat));
 				}
 				
 				// 출력
+				System.out.println("Arraylist ver.");
 				int a = 1;
-				System.out.println("\n------------------------------------------------------------");
+				System.out.println("------------------------------------------------------------");
 				System.out.println("\t\t성\t적\t표");
 				System.out.println("------------------------------------------------------------");
 				System.out.println(" 번호\t이름\t국어\t영어\t수학\t총점\t평균\t석차");
@@ -40,6 +43,20 @@ public class Main {
 				}
 				System.out.println("------------------------------------------------------------\n");
 				
+				// 출력
+				System.out.println("Treeset ver.");
+				int a1 = 1;
+				System.out.println("------------------------------------------------------------");
+				System.out.println("\t\t성\t적\t표");
+				System.out.println("------------------------------------------------------------");
+				System.out.println(" 번호\t이름\t국어\t영어\t수학\t총점\t평균\t석차");
+				System.out.println("------------------------------------------------------------");
+				for (Student temp : treeSet2) {
+					System.out.printf(" %d\t%s\t%d\t%d\t%d\t%d\t%.2f\t%d\n",
+							a1,temp.name,temp.kor,temp.eng,temp.mat,temp.total,temp.avg,a1);
+					a1++;
+				}
+				System.out.println("------------------------------------------------------------\n");
 				// 이전 데이터 제거 : 매회 5개 데이터만 출력
 				treeSet.removeAll(treeSet);			
 			}
